626ff115ad8e5b92ef945cb6956f2a3aa8af1478
Basil, Ugo <115357283+thebasilugo@users.noreply.github.com>
2023-10-28 08:15:04 +0000

    Delete front-end directory

--- front-end/internshippage.js
+++
@@ -1,9 +0,0 @@
-// function to show password
-function show_password() {
-  var x = document.getElementById("myPwd");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
-}
